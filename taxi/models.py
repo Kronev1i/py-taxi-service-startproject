f3c92d98055bbf153dcc339700ecb9c9ee340f2d
unknown <kgoncharenko83@gmail.com>
2026-04-23 16:32:35 +0200

    Added app-server for taxi and users

---
+++ taxi/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        "Manufacturer", on_delete=models.CASCADE, related_name="cars"
+    )
+    drivers = models.ManyToManyField("Driver", related_name="drivers")
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.username} - {self.license_number}"
